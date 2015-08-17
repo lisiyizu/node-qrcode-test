5666a410a0c414192d4ce49085fab0b6ca80aba1
Y-M-T <ymt1@wanglingdeMacBook-Air.local>
2015-08-17 12:19:17 +0800

    提交

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-//学习地址：http://blog.nodejitsu.com/npmawesome-qr-codes/
+//学习地址: http://blog.nodejitsu.com/npmawesome-qr-codes/
+//注意: 中文字符会有点问题
+var url = require('url');
 var qr = require('qr-image');  
 var express = require('express');
 var app = express();
 server = require('http').createServer(app);
 
-app.use('/svg/:text', function(req, res) {  
-  var code = qr.image(req.params.text, { type: 'svg',size:10, });
+app.get('/svg', function(req, res) {  
+  var code = qr.image(req.query.text||'HelloSvgCode', { type: 'svg',ec_level: 'H',margin:2 ,size:parseInt(req.query.size||10) });
   res.type('svg');
   code.pipe(res);
 });
 
-app.use('/png/:text', function(req,res){
-   var code = qr.image(req.params.text, { type: 'png', ec_level: 'H', size: 10, margin: 0 });
+app.get('/png', function(req,res){
+   var code = qr.image(req.query.text||'HelloPngCode', { type: 'png',parse_url:true, ec_level: 'H',margin:2, size: parseInt(req.query.size||10) });
    res.type('png'); 
    code.pipe(res);
 });
