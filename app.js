cc80d9a8fa53f28ab6944995cf7d74284d39f8e6
Y-M-T <ymt1@wanglingdeMacBook-Air.local>
2015-08-17 10:39:31 +0800

    增加svg 和 png的图片生产

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res) {  
-  var code = qr.image(new Date().toString(), { type: 'svg' });
+app.get('/svg/:text', function(req, res) {  
+  var code = qr.image(req.params.text, { type: 'svg' });
   res.type('svg');
   code.pipe(res);
+});
+
+app.get('/png/:text', function(req,res){
+   var code = qr.image(req.params.text, { type: 'png', ec_level: 'H', size: 10, margin: 0 });
+  //res.setHeader('Content-type', 'image/png');
+  res.type('png'); 
+  code.pipe(res);
+  // res.end();
 });
 
 app.listen(3000);  
